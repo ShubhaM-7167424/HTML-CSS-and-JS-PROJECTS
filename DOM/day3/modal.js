0aa51cd2d49afbf303d8fb379c1da02d6ce5b9f7
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-09-27 11:34:36 +0530

    popup or modal

---
+++ DOM/day3/modal.js
@@ -0,0 +1,7 @@
+document.getElementById('open-modal').addEventListener('click', function(){
+        document.getElementById('overlay').style.display = 'flex'
+})
+
+document.getElementById('close-modal').addEventListener('click', function(){
+        document.getElementById('overlay').style.display = 'none'
+})
