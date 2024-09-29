fd624e7747267b6c16d15c26b4d959d77c1e4c4d
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-09-29 10:53:31 +0530

    carousel

---
+++ DOM/day4/carousel.js
@@ -0,0 +1,15 @@
+let ml = 100;
+
+let slider = setInterval(() => {
+    document.getElementById("carousel").style.transition = "1s";
+    document.getElementById("carousel").style.marginLeft = -ml + "%";
+    ml = ml + 100;
+
+    if (ml > 400) {
+        setTimeout(() => {
+            document.getElementById("carousel").style.transition = "0s";
+            document.getElementById("carousel").style.marginLeft = "0%";
+            ml = 100;
+        }, 1000);
+    }
+}, 3000);
