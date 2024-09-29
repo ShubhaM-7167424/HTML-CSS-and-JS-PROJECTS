64cd39b9bde602fc857b29910ba8111d23c670ae
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-09-29 19:15:39 +0530

    scroll to section

---
+++ DOM/day5/scroll.js
@@ -0,0 +1,21 @@
+// document.getElementById('section3-link').addEventListener('click',()=>{
+//         document.getElementById('section3').scrollIntoView({behavior: "smooth"})
+// })
+
+// document.getElementById('section4-link').addEventListener('click', ()=>{
+//         document.getElementById('section4').scrollIntoView({behavior:'smooth'})
+// })
+
+// document.getElementById('section2-link').addEventListener('click', ()=>{
+//         document.getElementById('section2').scrollIntoView({behavior: 'smooth'})
+// })
+
+// document.getElementById('section1-link').addEventListener('click', ()=>{
+//         document.getElementById('section1').scrollIntoView({behavior: 'smooth'})
+// })
+
+function scrollToSection(sectionId) {
+        document.getElementById(sectionId).scrollIntoView({behavior: 'smooth'})
+}
+
+
