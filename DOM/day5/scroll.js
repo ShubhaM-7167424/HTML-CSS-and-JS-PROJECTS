ac6d2f4d2bf30e24ff7031310b81b6f776a83c71
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-09-29 20:19:58 +0530

    back to top button

--- DOM/day5/scroll.js
+++ DOM/day5/scroll.js
@@ -24,14 +24,25 @@
 // })
 
 window.addEventListener("scroll", () => {
+    // header color change on scroll logic
     if (window.scrollY >= 200) {
         document.getElementById("header").style.backgroundColor = "white";
     } else {
         document.getElementById("header").style.backgroundColor = "gray";
     }
+
+    // backtotop btn display on scroll logic
+    if (window.scrollY >= 800) {
+        document.getElementById("btt").style.display = "block";
+    } else {
+        document.getElementById("btt").style.display = "none";
+    }
 });
 
+document.getElementById("cs").addEventListener("click", () => {
+    window.scrollTo({ top: 800, behavior: "smooth" });
+});
 
-document.getElementById('cs').addEventListener('click',()=>{
-        window.scrollTo({top:800, behavior:'smooth'})
-})
+document.getElementById("btt").addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
